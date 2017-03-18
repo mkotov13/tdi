2e5eae08ca6137ed69c40afbdf9691d9fdfec4f8
mkotov13 <mkotov13@simons-rock.edu>
2017-03-18 18:14:44 -0400

    templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hi there"
+    user = {'name': 'Max'} # mock user
+    questions = [
+        {
+            'author': { 'name': 'Michael L.'},
+            'body': 'What"s the circumference of Earth on the equator?'
+        },
+        {
+            'author': { 'name': 'Christian M.'},
+            'body': 'How many years have the Data Incubator been around for?'
+        }
+    ]
+    return render_template('index.html',
+                            title='Home',
+                            user=user,
+                            questions=questions)
+
+@app.route('/testing')
+def testing():
+    return "Are we here?"
